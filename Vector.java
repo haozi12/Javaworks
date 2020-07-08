ef2511532b0c594fd42aa06ceab9ee99326c997a
haozi12 <62632197+haozi12@users.noreply.github.com>
2020-07-08 13:22:26 +0800

    Add files via upload

--- Vector.java
+++ Vector.java
@@ -1,8 +1,10 @@
+package com.haozi12;
+
 public class Vector {
     double x;
     double y;
     double z;
-    void creatVector(double x,double y, double z)//initialize the vector
+    void createVector(double x,double y, double z)//initialize the vector
     {
         this.x = x;
         this.y = y;
@@ -15,7 +17,7 @@ public class Vector {
         double x = vector1.x + vector2.x;
         double y = vector1.y + vector2.y;
         double z = vector1.z + vector2.z;
-        vector.creatVector(x,y,z);//initialize the result
+        vector.createVector(x,y,z);//initialize the result
         return vector;
     }
 
@@ -34,27 +36,36 @@ public class Vector {
 
     public static double getAngle(Vector vector1 , Vector vector2)
     {
-        double dotProduct = dotProduct(vector1,vector2);
+        double dotProduct = dotProduct(vector1, vector2);
         double cosValue = dotProduct / (vector1.getModule() * vector2.getModule());
         if (cosValue == 1.0000000000000002)
-        {   //the value of cosValue somehow larger than 1.0
-            //while using var / (Math.pow(var,0.5)*Math.pow(var,0.5))
-            return Math.acos((float)cosValue);
-        }
+            return Math.acos((float) cosValue);
         else
             return Math.acos(cosValue);
     }
 
-    public static void main(String[] args) //test class Vector and the commands in it
+    public static Vector multipliedByReal(double real, Vector vector)
     {
-        Vector a = new Vector();
-        Vector b = new Vector();
-        a.creatVector(0, 0, 1);
-        b.creatVector(1, 0, 0);
-        Vector c = Vector.add(a, b);
-        double d = Vector.dotProduct(a, b);
-        double A = Vector.getAngle(a, b);
-        double B = a.getModule();
-        System.out.println(c.x + " " + c.y + " " + c.z + " " + d + " " + A + " " + B + " ");
+        double a = real * vector.x;
+        double b = real * vector.y;
+        double c = real * vector.z;
+        Vector vector1 = new Vector();
+        vector1.createVector(a,b,c);
+        return vector1;
+    }
+
+    public static void showVector(Vector vector)
+    {
+        System.out.println(vector.x + " " + vector.y + " " + vector.z );
+    }
+
+    public static Vector crossProduct(Vector vector1, Vector vector2)
+    {
+        double a = vector1.y * vector2.z - vector1.z * vector2.y;
+        double b = vector1.z * vector2.x - vector1.x * vector2.z;
+        double c = vector1.x * vector2.y - vector1.y * vector2.x;
+        Vector vector = new Vector();
+        vector.createVector(a,b,c);
+        return vector;
     }
 }
